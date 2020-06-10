00d9e4a87947fdd3f85dd0b4173dba0402995dd5
Negar609 <58275316+Negar609@users.noreply.github.com>
2020-06-09 23:40:18 -0700

    Add files via upload

---
+++ src/js/sale-tax.teacher.js
@@ -0,0 +1,78 @@
+/* global document, salesTaxData */
+
+function calculateTotal(salesTax, price) {
+  return (price * salesTax) + parseFloat(price);
+}
+
+function totalTax(taxes) {
+  // combine federal and/or provincial sales tax into a total sales tax string
+  if (taxes.length === 1) return taxes[0].tax;
+  return parseFloat(taxes[0].tax + taxes[1].tax).toFixed(2);
+}
+
+function populateDropdownJQ(provinces) {
+  $.each(provinces, (provinceAbbr) => {
+    $('#provinces')
+      .append(`<option value="${totalTax(provinces[provinceAbbr].taxes)}">
+        ${provinces[provinceAbbr].name}
+      </option>`);
+  });
+}
+
+function populateDropdownJS() {
+  const provinceData = Object.values(salesTaxData.provinces);
+  const html = provinceData.map((item) => `<option value="${totalTax(item.taxes)}">
+    ${item.name}
+  </option>`).join('');
+  document.querySelector('#provinces').innerHTML += html;
+}
+
+function bindTaxDomJQ() {
+  $('#provinces, #price').change(() => {
+    const tax = $('#provinces').val();
+    const price = $('#price').val();
+
+    $('#pizzaCost').html(calculateTotal(tax, price));
+  });
+}
+
+function bindTaxDomJS() {
+  // convert from DOM list to Array
+  Array.from(document.querySelectorAll('#provinces, #price')).forEach((dom) => {
+    dom.addEventListener('change', () => {
+      const tax = document.querySelector('#provinces').value;
+      const price = document.querySelector('#price').value;
+
+      document.querySelector('#pizzaCost').innerHTML = calculateTotal(tax, price);
+    });
+  });
+}
+
+function pizzaSalesJQ() {
+  bindTaxDomJQ();
+  populateDropdownJQ(salesTaxData.provinces);
+}
+
+function pizzaSalesJS() {
+  bindTaxDomJS();
+  populateDropdownJS();
+}
+
+function pizzaSalesAjax() {
+  $.ajax({
+    url: 'rates.json',
+    success: (result) => {
+      bindTaxDomJQ();
+      populateDropdownJQ(result.provinces);
+    },
+  });
+}
+
+// If Node.js then export as public
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    pizzaSalesJQ,
+    pizzaSalesJS,
+    pizzaSalesAjax,
+  };
+}
